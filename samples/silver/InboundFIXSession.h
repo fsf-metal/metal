524ea5830932611f452f1833ce2b0b50b8879ba2
Jean-Cedric JOLLANT <jc@vps86036.ovh.net>
2014-09-06 00:35:54 +0200

    Remove unused stuff

---
+++ samples/silver/InboundFIXSession.h
@@ -0,0 +1,16 @@
+#ifndef __INBOUNDFIXSESSION_H
+#define __INBOUNDFIXSESSION_H
+
+
+class InboundFIXSession {
+	public:
+		void start() {
+			std::cout << "start() invoked but not implemted" << std::endl;
+		}
+
+		void stop() {
+			std::cout << "stop() invoked but not implemted" << std::endl;
+		}
+};
+
+#endif // __INBOUNDFIXSESSION_H
