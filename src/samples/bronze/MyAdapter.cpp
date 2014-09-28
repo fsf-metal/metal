9885646bdc2f420a5c2e7faa3ccd662abec311ca
Fred Masto <fred@jollant.net>
2014-09-28 09:05:32 +0200

    Added Details to Execution Report output

--- src/samples/bronze/MyAdapter.cpp
+++ src/samples/bronze/MyAdapter.cpp
@@ -18,12 +18,44 @@ MyAdapter::~MyAdapter() {
 }
 
 void MyAdapter::recv( const Metal::ExecutionReport &er) {
+	FIX::OrderID orderID;
+	er.getField(orderID);
+
+	FIX::ExecID execID;
+	er.getField(execID);
+
+	FIX::ExecType execType;
+	er.getField(execType);
+
 	FIX::ClOrdID clOrdID;
 	er.getField( clOrdID);
+
 	FIX::OrdStatus ordStatus;
 	er.getField( ordStatus);
 
+	FIX::Symbol symbol;
+	er.getField(symbol);
+
+	FIX::Side side;
+	er.getField(side);
+
+	FIX::LeavesQty leavesQty;
+	er.getField(leavesQty);
+
+	FIX::CumQty cumQty;
+	er.getField(cumQty);
+
 	// report and mimic prompt
-	std::cout << "\nExecution Report: order " << clOrdID << " is \"" << Metal::Translator::field(ordStatus) << "\"\n> " << std::flush;
+	std::cout << "\nExecution Report: clOrdID=" << clOrdID;
+	std::cout << " status=" << Metal::Translator::field(ordStatus);
+	std::cout << " orderID=" << orderID;
+	std::cout << " execID=" << execID;
+	std::cout << " execType=" << execType;
+	std::cout << " symbol=" << symbol;
+	std::cout << " side=" << side;
+	std::cout << " leavesQty=" << leavesQty;
+	std::cout << " cumQty=" << cumQty;
+
+	std::cout << "\n> " << std::flush;
 }
 
